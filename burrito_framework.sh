44e484071b5b93ec3fc0178c7f7d4135331d9841
elBichon <reuglewiczjeanedouard@gmail.com>
2017-08-03 22:41:20 +0200

    Update burrito_framework.sh

--- burrito_framework.sh
+++ burrito_framework.sh
@@ -1,11 +1,12 @@
-
 #!/bin/bash
 
 #declare variables for manipulation use
 new="create_project";
+security="add_security";
 entity="new_entity";
 module="new_module";
 object="new_object";
+
 
 #creation of a new project
 if [ $1 = $new ]
@@ -30,6 +31,7 @@ then
 		mkdir $name_project/build_in/static/required
 		mkdir $name_project/build_in/static/form
 		mkdir $name_project/build_in/entity
+		touch $name_project/build_in/static/style/style.css
 		touch $name_project/build_in/entity/database.sql
 		touch $name_project/build_in/functions.php
 		touch $name_project/build_in/form/upload.php
@@ -48,19 +50,21 @@ then
 <!DOCTYPE html>
 <html>
     <head>
+	<link <link href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet'>
         <meta charset='fr' />
         <link rel='icon' type='image/ico' href='https://www.w3schools.com/images/w3schools_green.jpg' />
-        <link rel='stylesheet' href='style.css' />
+        <link rel='stylesheet' href='build_in/static/style/style.css' />
         <title>index</title>
     </head>
     <body>
         <div id='bloc_page'>
             <header>
-			<a href='https://github.com/you'><img style='position: absolute; top: 0; right: 0; border: 0;' src='https://github.com/elBichon/burrito_framework' alt='Fork me on GitHub' data-canonical-src='https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png'></a>
 			<?php "'$titre_principal'" = 'titre principal du module';	
 			      "'$titre2'" = 'titre secondaire du module';
 			 ?>
-			<?php require('./build_in/connection.php'); ?>
+			<?php require('mapping.php'); ?>
+			<?php require('build_in/functions.php'); ?>
+			<?php require('build_in/connection.php'); ?>
 			<?php require('build_in/static/required/header.php'); ?>
                 </div>           
                 <nav>
@@ -78,6 +82,8 @@ then
 			<?php require('build_in/static/required/footer.php'); ?>
             </footer>
         </div>
+    <script src='https://code.jquery.com/jquery-1.11.2.min.js'></script>
+    <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>
     </body>
 </html>
 " >> $name_project/index.php
@@ -90,11 +96,16 @@ grant all privileges on "$name_project".* to '"$name_project"_user'@'localhost' 
 		echo "
 ErrorDocument 404 http://localhost/"$name_project"/erreurs/404.php
 " >> $name_project/.htaccess
+	echo " 
+<ul>
+	<li><a href=<?php echo $index; ?>>Home</a></li>
+</ul>
+" >> $name_project/nav.php
 		echo "
 <?php
-	echo 'erreur 404';
+	"'$index'"="'/'"$name_project;
 ?>
-" >> $name_project/erreurs/404.php
+" >> $name_project/mapping.php
 	echo "
 <div id='titre_principal'>
 	<h1><?php echo "'$titre_principal'" ?></h1>    
@@ -103,12 +114,10 @@ ErrorDocument 404 http://localhost/"$name_project"/erreurs/404.php
 " >> $name_project/build_in/static/required/header.php
 	echo " 
 <ul>
-	<li><a href='#'>Liens</a></li>
+	<li><a href=<?php echo "'$index'"; ?>>Home</a></li>
 </ul>
 " >> $name_project/build_in/static/required/nav.php
-	echo "<p>Proudly created with: Pink Hippo</p>
-  <p>Contact information: <a href="mailto:someone@example.com">
-  someone@example.com</a>.</p>
+	echo "<p>Proudly created with: Burrito Framework</p>
 " >> $name_project/build_in/static/required/footer.php
 	echo "<?php
     try
@@ -151,15 +160,15 @@ ErrorDocument 404 http://localhost/"$name_project"/erreurs/404.php
 " >> $name_project/build_in/static/form/uploadSuite.php
 		echo '<?php
     function code($texte){
-                $texte = str_replace(":D ", "<img src="./images/bbcode/smileys/heureux.png" title="heureux" alt="heureux" />", $texte);
-                $texte = str_replace(":lol: ", "<img src="./images/bbcode/smileys/lol.png" title="lol" alt="lol" />", $texte);
-                $texte = str_replace(":triste:", "<img src="./images/bbcode/smileys/triste.png" title="triste" alt="triste" />", $texte);
-                $texte = str_replace(":frime:", "<img src="./images/bbcode/smileys/cool.png" title="cool" alt="cool" />", $texte);
-                $texte = str_replace(":rire:", "<img src="./images/bbcode/smileys/rire.png" title="rire" alt="rire" />", $texte);
-                $texte = str_replace(":s", "<img src="./images/bbcode/smileys/confus.png" title="confus" alt="confus" />", $texte);
-                $texte = str_replace(":O", "<img src="./images/bbcode/smileys/choc.png" title="choc" alt="choc" />", $texte);
-                $texte = str_replace(":question:", "<img src="./images/bbcode/smileys/question.png" title="?" alt="?" />", $texte);
-                $texte = str_replace(":exclamation:", "<img src="./images/bbcode/smileys/exclamation.png" title="!" alt="!" />", $texte);
+                $texte = str_replace(":D ", "<img src="../images/bbcode/smileys/heureux.png" title="heureux" alt="heureux" />", $texte);
+                $texte = str_replace(":lol: ", "<img src="../images/bbcode/smileys/lol.png" title="lol" alt="lol" />", $texte);
+                $texte = str_replace(":triste:", "<img src="../images/bbcode/smileys/triste.png" title="triste" alt="triste" />", $texte);
+                $texte = str_replace(":frime:", "<img src="../images/bbcode/smileys/cool.png" title="cool" alt="cool" />", $texte);
+                $texte = str_replace(":rire:", "<img src="../images/bbcode/smileys/rire.png" title="rire" alt="rire" />", $texte);
+                $texte = str_replace(":s", "<img src="../images/bbcode/smileys/confus.png" title="confus" alt="confus" />", $texte);
+                $texte = str_replace(":O", "<img src="../images/bbcode/smileys/choc.png" title="choc" alt="choc" />", $texte);
+                $texte = str_replace(":question:", "<img src="../images/bbcode/smileys/question.png" title="?" alt="?" />", $texte);
+                $texte = str_replace(":exclamation:", "<img src="../images/bbcode/smileys/exclamation.png" title="!" alt="!" />", $texte);
                 $texte = preg_replace("`\[g\](.+)\[/g\]`isU", "<strong>$1</strong>", $texte); 
                 $texte = preg_replace("`\[i\](.+)\[/i\]`isU", "<em>$1</em>", $texte);
                 $texte = preg_replace("`\[s\](.+)\[/s\]`isU", "<u>$1</u>", $texte);
@@ -169,21 +178,252 @@ ErrorDocument 404 http://localhost/"$name_project"/erreurs/404.php
     ?>
 ' >> $name_project/build_in/functions.php
 	echo 'Project has been created, time to have fun ;)'
+echo '
+
+header{
+padding: 0px;
+margin: 0px;
+border: none;
+width: 100%;
+height: 50px;
+background-color: #D3D3D3;
+} 
+footer{
+padding: 0px;
+margin: 0px;
+border: none;
+width: 100%;
+height: 50px;
+background-color: #D3D3D3;
+} 
+body{
+text-align: center;
+}
+'>> $name_project/build_in/static/style/style.css
+#creation of a new module which will fulfill a precise duty
+elif [ $1 = $security ]
+then
+        echo "Creation of security bundle"
+        name_project=$2
+		mkdir $name_project/security
+		touch $name_project/security/connexion.php
+		touch $name_project/security/deconnexion.php
+		touch $name_project/security/connect_form.php
+		touch $name_project/security/functions.php
+	echo "<?php
+    session_start();
+    ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='fr' />
+        <link rel='icon' type='image/ico' href='https://www.w3schools.com/images/w3schools_green.jpg' />
+        <link rel='stylesheet' href='style.css' />
+        <title>index</title>
+    </head>
+    <body>
+    <?php
+    
+    //echappement des champs recus
+        "'$login'" = htmlspecialchars("'$_POST'"['login']);
+        "'$pass_hache'" = sha1(htmlspecialchars("'$_POST'"['password']));
+    
+    //verification que les champs sont remplis
+        if (empty("'$login'") || empty("'$pass_hache'")){
+            "'$connexion_erreur'" = 'Vous devez renseigner un login et un mot de passe';
+            "'$retour'" = ' pour revenir à la page  accueil';
+            echo "'$connexion_erreur'";
+            echo '</br>';
+            echo "'$retour'";
+        }
+        else{
+            
+            //verification que le compte existe
+            "'$req'" = "'$bdd'"->prepare('SELECT id FROM identification WHERE login = :login AND pass = :pass');
+            "'$req'"->execute(array(
+                                'login' => "'$login'",
+                                'pass' => "'$pass_hache'"));
+            "'$resultat'" = "'$req'"->fetch();
+        
+        if (!"'$resultat'"){
+            
+            //si le champ n existe pas
+            echo 'Mauvais identifiant ou mot de passe !';
+        }
+        else{
+            
+            //si le compte existe creation de la session 
+            session_start();
+            "'$_SESSION'"['id'] = "'$resultat'"['id'];
+            "'$id'" = "'$_SESSION'"['id'];
+            "'$_SESSION'"['login'] = "'$login'";
+            echo 'Bonjour ' . "'$_SESSION'"['login'];
+            <header>
+			<?php "'$titre_principal'" = 'titre principal du module';	
+			      "'$titre2'" = 'titre secondaire du module';
+			 ?>
+			<?php require('../mapping.php'); ?>
+			<?php require('../build_in/functions.php'); ?>
+			<?php require('../build_in/connection.php'); ?>
+			<?php require('../build_in/static/required/header.php'); ?>
+                </div>           
+                <nav>
+                <nav>
+			<?php require('../build_in/static/required/nav.php'); ?>
+                </nav>
+
+            
+            
+        }
+    }
+?>
+            <footer>
+			<?php require('../build_in/static/required/footer.php'); ?>
+            </footer>
+        </div>
+    </body>
+</html>
+" >> $name_project/security/connexion.php
+
+	echo "
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='fr' />
+        <link rel='icon' type='image/ico' href='https://www.w3schools.com/images/w3schools_green.jpg' />
+        <link rel='stylesheet' href='style.css' />
+        <title>index</title>
+    </head>
+    <body>
+        <div id='bloc_page'>
+            <header>
+			<?php "'$titre_principal'" = 'titre principal du module';	
+			      "'$titre2'" = 'titre secondaire du module';
+			 ?>
+			<?php require('../mapping.php'); ?>
+			<?php require('../build_in/functions.php'); ?>
+			<?php require('../build_in/connection.php'); ?>
+			<?php require('../build_in/static/required/header.php'); ?>
+                </div>           
+                <nav>
+                <nav>
+			<?php require('../build_in/static/required/nav.php'); ?>
+                </nav>
+
+<?php
+    session_start();
+    
+    // Suppression des variables de session et de la session
+    $_SESSION = array();
+    session_destroy();
+    
+    // Suppression des cookies de connexion automatique
+    setcookie('login', '');
+    setcookie('pass_hache', '');
+?>
+            <footer>
+			<?php require('../build_in/static/required/footer.php'); ?>
+            </footer>
+        </div>
+    </body>
+</html>
+" >> $name_project/security/deconnexion.php
 
 #creation of a new entity to load in the database
 elif [ $1 = $entity ]
 then
         echo "Creation of a new entity"
-c
+        name_project=$2
+        name_table=$3
+        sql=".sql"
+        name_new_table="$name_table""$sql"
+        
 	echo $name_table
-	echo 'Creation of the new table :' "'$name_table'"
-		touch $name_project/build_in/entity/$name_table
+	echo 'Creation of the new table :' "'$name_new_table'"
+		touch $name_project/build_in/entity/$name_new_table
 	echo 'Creating the fields of the table'
-	echo "drop table if exists "'$name_table'";
-create table "'$name_table'" (
+	echo "drop table if exists $name_table;
+create table $name_table (
 id integer not null primary key auto_increment,
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
-" >> $name_project/build_in/entity/$name_table
+" >> $name_project/build_in/entity/$name_new_table
+
+
+	echo "
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='fr' />
+        <link rel='icon' type='image/ico' href='https://www.w3schools.com/images/w3schools_green.jpg' />
+        <link rel='stylesheet' href='style.css' />
+        <title>index</title>
+    </head>
+    <body>
+            <header>
+			<?php "'$titre_principal'" = 'titre principal du module';	
+			      "'$titre2'" = 'titre secondaire du module';
+			 ?>
+			<?php require('../mapping.php'); ?>
+			<?php require('../build_in/functions.php'); ?>
+			<?php require('../build_in/static/required/header.php'); ?>
+                </div>           
+<?php
+//si l utilisateur est deja connecte
+    if (isset("'$_SESSION'"['login'])) {
+        echo 'Il y a eu un problème de connexion, vous ne devriez pas voir ceci';
+        echo '</br>';
+        echo 'rendez vous <a href='deconnexion.php'>ICI</a>';
+       }
+       
+       //formulaire de connexion
+   else {
+    require('../build_in/connection.php');
+    echo '<section id='connexion'>';
+        echo '<h3>Connexion</h3></br>';
+        echo '<div class="">';
+            echo '<form action='connexion.php' method='post'>';
+            echo '<div id=loginConnexion'>';
+                echo '<label for='login'>Login</label>';
+                echo '<input type='text' class='form-control' id='champLogin' placeholder='login' name='login' required>';
+            echo '</div>';
+            echo '<div class='error-message'></div></br>';
+            echo '<div id='passwordConnexion'>';
+                echo '<label for='password'>Mot de passe</label>';
+                echo '<input type='password' class='form-control' placeholder='password' id='champPassword' name='password' required>';
+            echo '</div>';
+            echo '<div class='error-message'></div></br>';
+            echo '<div class='button'>';
+                echo '<button type='submit' id='envoyerConnexion' class='btn btn-default'>Se connecter</button>';
+            echo '</div>';
+            echo '</form>';
+        echo '</div>';
+    echo '</section>';
+            <footer>
+			<?php require('../build_in/static/required/footer.php'); ?>
+            </footer>
+        </div>
+    </body>
+</html>
+" >> $name_project/security/connect_form.php
+
+#creation of a new entity to load in the database
+elif [ $1 = $entity ]
+then
+        echo "Creation of a new entity"
+        name_project=$2
+        name_table=$3
+        sql=".sql"
+        name_new_table="$name_table""$sql"
+        
+	echo $name_table
+	echo 'Creation of the new table :' "'$name_new_table'"
+		touch $name_project/build_in/entity/$name_new_table
+	echo 'Creating the fields of the table'
+	echo "drop table if exists $name_table;
+create table $name_table (
+id integer not null primary key auto_increment,
+) engine=innodb character set utf8 collate utf8_unicode_ci;
+" >> $name_project/build_in/entity/$name_new_table
 
 #creation of a new module which will fulfill a precise duty
 elif [ $1 = $module ]
@@ -218,22 +458,24 @@ then
 			<?php "'$titre_principal'" = 'titre principal du module';	
 			      "'$titre2'" = 'titre secondaire du module';
 			 ?>
-			<?php require('./build_in/connection.php'); ?>
-			<?php require('./build_in/static/required/header.php'); ?>
+			<?php require('../mapping.php'); ?>
+			<?php require('../build_in/functions.php'); ?>
+			<?php require('../build_in/connection.php'); ?>
+			<?php require('../build_in/static/required/header.php'); ?>
                 </div>           
                 <nav>
-			<?php require('./build_in/static/required/nav.php'); ?>
+			<?php require('../build_in/static/required/nav.php'); ?>
                 </nav>
             </header>
             <section>
 	    </section>
             <footer>
-			<?php require('./build_in/static/required/footer.php'); ?>
+			<?php require('../build_in/static/required/footer.php'); ?>
             </footer>
         </div>
     </body>
 </html>
-" >> $name_project/index.php
+" >> $name_project/$name_module/$name_main_module
 
 #creation of a new object which will be used by a specific module
 elif [ $1 = $object ]
@@ -246,7 +488,7 @@ then
         name_module=$4
 		touch $name_project/$name_module/objects/$name_object
 		echo "<?php
-    class "'$object'"
+    class "$object"
     {
         private "'$_id'",
         
